996084b597665889f648574d33bdc38f22643818
PlanB <city7310@naver.com>
2017-10-06 00:06:39 +0900

    [ADD] setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tourapi',
+      version='0.1',
+      url='https://github.com/JoMingyu/TourAPI',
+      license='MIT',
+      author='PlanB',
+      author_email='city7310@naver.com',
+      description='Helps TourAPI parsing',
+      packages=['tourapi'],
+      install_requires=['bs4'])
