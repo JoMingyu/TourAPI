f9427a8f554b2b2fcdd103b96681c726193b8240
JoMingyu <city7310@naver.com>
2017-10-07 20:20:45 +0900

    New version 2.0 Release

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
 setup(name='tourapi',
-      version='0.1.1',
+      version='0.2',
       url='https://github.com/JoMingyu/TourAPI',
       license='MIT',
       author='PlanB',
       author_email='city7310@naver.com',
       description='Helps TourAPI parsing',
-      packages=['tourapi'],
-      install_requires=['bs4'])
+      packages=['tourapi'])
