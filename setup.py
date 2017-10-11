1f35ce9d31f37ee803a77f32557681061932d4fc
PlanB <city7310@naver.com>
2017-10-11 11:08:35 +0900

    New version 0.2.1 Release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tourapi',
-      version='0.2',
+      version='0.2.1',
       url='https://github.com/JoMingyu/TourAPI',
       license='MIT',
       author='PlanB',
